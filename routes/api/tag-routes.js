6ffd7b0110d49ddc866200baef32886b82a723fb
JohnKihm <john.a.kihm@gmail.com>
2024-06-30 14:47:16 -0500

    added get one, get all, and delete routes to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,10 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    
+    const tagData = await Tag.findAll({
+      include: [Product]
+    });
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -17,7 +20,16 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [Product]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,7 +56,18 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
