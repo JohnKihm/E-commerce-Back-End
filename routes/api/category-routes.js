c908ca11636b9bf9d2d0c27cf21c0c82e3e1345b
JohnKihm <john.a.kihm@gmail.com>
2024-07-01 00:32:58 -0500

    added post route to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,7 +38,8 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
   } catch (err) {
     res.status(500).json(err);
   }
